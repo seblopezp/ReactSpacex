46bd0a1795d3c4c16a9fc82ced0570d24a76f649
seblopezp <seblopezp@gmail.com>
2022-04-22 20:20:58 -0400

    ADD: componente loading

---
+++ src/pages/components/loading/loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { CircularProgress } from "@mui/material";
+
+const Loading = (props) => {
+  console.log(props);
+  return (
+    <p>loading...</p>
+  );
+};
+
+export default Loading;
