378cd02d3838a782f8802f143c4337d26fd80416
seblopezp <seblopezp@gmail.com>
2022-05-06 09:44:48 -0400

    UPDATE: se lleva logica de fetch a custom hook

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,48 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Grid } from "@mui/material";
 import { constants } from "../utils/constants";
 import Loading from "../components/loading/loading";
 import LaunchCard from "../components/launchCard/launchCard";
+import { useFetch } from "../hooks/useFetch";
 const Home = () => {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [launches, setLaunches] = useState([]);
   const { SPACEX_API_URL } = constants;
-  useEffect(() => {
-    fetch(`${SPACEX_API_URL}/launches`)
-      .then((res) => res.json())
-      .then(
-        (data) => {
-          setIsLoaded(true);
-          setLaunches(data);
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      );
-  }, []);
-
-  const LaunchesGrid = () => {
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <Loading />;
-    } else {
-      return (
-        <>
-          <Grid container spacing={2}>
-            {launches.map((launch) => (
-              <Grid item xs={6} sm={4} md={4} key={launch.id}>
-                <LaunchCard launch={launch} />
-              </Grid>
-            ))}
-          </Grid>
-        </>
-      );
-    }
-  };
-
-  return <LaunchesGrid />;
+  const { loading, data } = useFetch(`${SPACEX_API_URL}/launches`);
+ 
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <Grid container spacing={2}>
+          {data.map((launch) => (
+            <Grid item xs={6} sm={4} md={4} key={launch.id}>
+              <LaunchCard launch={launch} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </>
+  );
 };
+
 export default Home;
