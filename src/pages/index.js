a6a85e6df5d79b5104c20bc207fb17099b1ace4c
seblopezp <seblopezp@gmail.com>
2022-05-06 09:45:49 -0400

    UPDATE: se actualiz render de componente

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SearchBar from "./components/searchBar/searchBar";
 import Home from "./Home/home";
 import Launch from "./Launch/launch";
 const Pages = () => {
   return (
     <BrowserRouter>
+    <SearchBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/launch/:id" element={<Launch />} />
