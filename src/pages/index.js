5e2afa00b96e3ee97b88b4abd36b7f09f68a5404
seblopezp <seblopezp@gmail.com>
2022-05-18 09:08:24 -0400

    UPDATE: se agregan nuevas rutas

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchBar from "./components/searchBar/searchBar";
 import Home from "./Home/home";
 import Launch from "./Launch/launch";
+import { LatestLaunches } from "./latestLaunches/LatestLaunches";
+import { NextLaunch } from "./nextLaunch/NextLaunch";
+import { UpcomingLaunch } from "./upcomingLaunch/UpcomingLaunch";
 const Pages = () => {
   return (
     <BrowserRouter>
-    <SearchBar />
+      <SearchBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/launch/:id" element={<Launch />} />
+        <Route path="launch/:id" element={<Launch />} />
+        <Route path="latest" element={<LatestLaunches />} />
+        <Route path="upcoming" element={<UpcomingLaunch />} />
+        <Route path="next" element={<NextLaunch />} />
       </Routes>
     </BrowserRouter>
   );
